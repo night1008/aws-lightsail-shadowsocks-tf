e1d86eef8e136eca7a13b3a17f1f2eca9a7f8a20
night1008 <1179039379@qq.com>
2022-02-26 17:42:45 +0800

    <feat>: add ss_url to output

--- lightsail-shadowsocks/main.tf
+++ lightsail-shadowsocks/main.tf
@@ -80,9 +80,14 @@ resource "aws_lightsail_instance_public_ports" "instance" {
 # }
 
 resource "alicloud_oss_bucket_object" "object" {
-  bucket  = var.output_oss_bucket
-  key     = local.output_oss_object_key
-  content = jsonencode({ "shadowsocks_config" = local.shadowsocks_libev_config, "public_ip_address" = aws_lightsail_instance.instance.public_ip_address, "static_ip" = var.config.create_static_ip ? aws_lightsail_static_ip.instance[0].ip_address : "" })
+  bucket = var.output_oss_bucket
+  key    = local.output_oss_object_key
+  content = jsonencode({
+    "shadowsocks_config" = local.shadowsocks_libev_config,
+    "public_ip_address"  = aws_lightsail_instance.instance.public_ip_address,
+    "static_ip"          = var.config.create_static_ip ? aws_lightsail_static_ip.instance[0].ip_address : ""
+    "ss_url"             = format("ss://%s#%s", base64encode(format("%s:%s@%s:%d", var.config.shadowsocks_libev_method, random_password.password.result, var.config.create_static_ip ? aws_lightsail_static_ip.instance[0].ip_address : aws_lightsail_instance.instance.public_ip_address, var.config.shadowsocks_libev_port)), format("%s-%s", var.config.region, var.config.instance_name))
+  })
 
   provisioner "local-exec" {
     command = "./download-oss-file.sh ${var.output_oss_bucket} ${local.output_oss_object_key} ${local.local_output_file_name}"
