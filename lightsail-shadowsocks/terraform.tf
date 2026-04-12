37351e996109b6107fcb6b3c2b0694b2ebc13961
night <1179039379@qq.com>
2026-04-12 15:31:31 +0800

    upgrade provider version

--- lightsail-shadowsocks/terraform.tf
+++ lightsail-shadowsocks/terraform.tf
@@ -2,12 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 6.40"
     }
 
     alicloud = {
       source  = "aliyun/alicloud"
-      version = "~> 1.157.0"
+      version = "~> 1.275"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8"
     }
   }
 }
--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,20 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.40"
+    }
+
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "~> 1.275"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.8"
+    }
+  }
+
   backend "oss" {}
 }
